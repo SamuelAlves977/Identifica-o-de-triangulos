bc8f6ce41dd8d1db6ad43a315d3bb966363586cb
Samuel Alves Silva <samuelalvessilva255@gmail.com>
2022-04-19 11:03:46 -0300

    commit inicial

---
+++ identificandoTriangulos/src/identificandotriangulos/IdentificandoTriangulos.java
@@ -0,0 +1,14 @@
+
+/*
+Elaborar um programa que aponte que tipo de triângulo corresponde os dados de entrada do usuário, sabendo-se que:
+Triângulo Escaleno – Lados e ângulos são diferentes
+Triângulo Isósceles – Dois lados iguais e ângulos opostos a esses lados iguais
+Triângulo Equilátero – Lados e ângulos iguais
+*/
+package identificandotriangulos;
+public class IdentificandoTriangulos {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
